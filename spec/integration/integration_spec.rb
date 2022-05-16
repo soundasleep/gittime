c649e87a36a0c96ad2df2add6ecb9a22b7a48f4a
Jevon Wright <jevon@jevon.org>
2022-05-16 12:00:03 +1200

    Implement git path filtering using only: config

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -7,6 +7,9 @@ describe "integration", type: :integration do
 
   let(:filenames) { result.map { |result| result[:filename] } }
 
+  let(:blocks) { result.select { |result| result[:filename] == "blocks.csv" }.only }
+  let(:lines) { blocks[:data].size }
+
   it "can be run against a repository" do
     expect(result).to_not eq nil
     expect(filenames).to include "revisions.csv"
@@ -16,10 +19,49 @@ describe "integration", type: :integration do
     expect(filenames).to include "work-by-month.csv"
   end
 
-  let(:work_by_month) { result.select { |result| result[:filename] == "work-by-month.csv" }.only }
-  let(:lines) { work_by_month[:data].size }
-
-  it "has calculated at least ten months of work" do
+  it "has calculated at least ten blocks of work" do
     expect(lines).to be >= 10
+  end
+
+  describe "a local config path" do
+    let(:config) { "#{File.dirname(__FILE__)}/#{config_filename}" }
+    let(:output_path) { "../../output" }
+    let(:options) { default_options.merge({ config: config, output: output_path }) }
+
+    describe "just a git repository" do
+      let(:config_filename) { "config.git.yml" }
+
+      it "can be run against a repository" do
+        expect(result).to_not eq nil
+        expect(filenames).to include "revisions.csv"
+        expect(filenames).to include "revisions-with-authors.csv"
+        expect(filenames).to include "blocks.csv"
+        expect(filenames).to include "blocks-by-month.csv"
+        expect(filenames).to include "work-by-month.csv"
+      end
+
+      it "has calculated at least four blocks of work" do
+        # This is less than above because this is only looking at a single git repo
+        expect(lines).to be >= 4
+      end
+    end
+
+    describe "path-based filtering" do
+      let(:config_filename) { "config.git.path-filtering.yml" }
+
+      it "can be run against a repository" do
+        expect(result).to_not eq nil
+        expect(filenames).to include "revisions.csv"
+        expect(filenames).to include "revisions-with-authors.csv"
+        expect(filenames).to include "blocks.csv"
+        expect(filenames).to include "blocks-by-month.csv"
+        expect(filenames).to include "work-by-month.csv"
+      end
+
+      it "has calculated no more than three blocks of work" do
+        # Path filtering should have reduced the amount of work tracked
+        expect(lines).to be <= 3
+      end
+    end
   end
 end
