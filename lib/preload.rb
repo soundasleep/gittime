461e43aadd5d7f8f8f85d64e950c81eb5e4ba4cf
Jevon Wright <jevon@jevon.org>
2026-06-25 11:08:18 +1200

    Update to Bundler 4.0

--- lib/preload.rb
+++ lib/preload.rb
@@ -1,15 +1,24 @@
 $LOAD_PATH << ENV['GEM_HOME'] if ENV['GEM_HOME']
 
 require "active_support"
+require "active_support/core_ext"
 
-ActiveSupport::Dependencies.autoload_paths = [
+require_relative "monkey_patches"
+require_relative "options"
+require_relative "logging"
+require_relative "deep_merge"
+
+# alternative to ActiveSupport::Dependencies.autoload_paths,
+# which no longer seems to work?
+load_everything_in = [
   File.join(File.dirname(__FILE__), '..', 'lib'),
   File.join(File.dirname(__FILE__), '..', 'helpers'),
   File.join(File.dirname(__FILE__), '..', 'model'),
   File.join(File.dirname(__FILE__), '..', 'services'),
 ]
 
-require_relative "monkey_patches"
-require_relative "options"
-require_relative "logging"
-require_relative "deep_merge"
+load_everything_in.each do |path|
+  Dir[File.join(path, "*.rb")].each do |file|
+    require file
+  end
+end
